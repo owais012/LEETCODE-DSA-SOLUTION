273b786649acc0f446ab19948cc956a07661fdfd
owais012 <146368386+owais012@users.noreply.github.com>
2024-09-07 18:40:58 +0530

    Time: 1900 ms (5.11%), Space: 46 MB (8.29%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
@@ -1,34 +1,9 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
 class Solution{ 
     boolean isPresent = false;
+
     public void dfs(TreeNode root, String s, String head){
         s += String.valueOf(root.val);
-        System.out.println(s);
+
         if(s.length() >= head.length()){
             String temp = "";
 
@@ -50,17 +25,18 @@ class Solution{
             dfs(root.right, s,head);
         }
 
-        
     }
 
     public boolean isSubPath(ListNode head, TreeNode root) {
         String listHead = "";
         var temp = head;
 
+        // 1 4 2 6 in case of second example 
         while(temp != null){
             listHead += String.valueOf(temp.val);
             temp = temp.next;
         }
+        
         dfs(root, "", listHead);
         return isPresent;
     }
