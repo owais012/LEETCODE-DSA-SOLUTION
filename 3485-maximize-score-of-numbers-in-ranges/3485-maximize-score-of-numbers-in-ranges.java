a811c66e814be20449f3bc84e7b27fa4e71d1590
owais012 <146368386+owais012@users.noreply.github.com>
2024-09-08 10:24:52 +0530

    Time: 48 ms (100%), Space: 63.9 MB (100%) - LeetHub

--- 3485-maximize-score-of-numbers-in-ranges/3485-maximize-score-of-numbers-in-ranges.java
+++ 3485-maximize-score-of-numbers-in-ranges/3485-maximize-score-of-numbers-in-ranges.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean isPossible(long d, int[]start, int D){
         long prev =  start[0]; 
-        // if(d > start[0] + D) return false;
 
         for(int i = 1; i < start.length; i++){
             long st = (long) start[i];
             long e =  ((long)start[i]) + D;
-            // System.out.println(prev + d);
-            // System.out.println(st + " " + e);
 
             if(prev+d > e){
                 return false;
             }
-
+    
             if(prev+d < st)
                 prev = st;
             else prev += d;
@@ -26,15 +23,6 @@ class Solution {
         Arrays.sort(start);
         int n = start.length;
 
-        int[][]intervals = new int[n][2];
-
-        for(int i = 0; i < n; i++){
-            intervals[i][0] = start[i];
-            intervals[i][1] = start[i]+d;
-        }
-
-        Arrays.sort(intervals, (a,b )->(a[0]- b[0]));
-        // System.out.println(isPossible(1000_000_000, start, d));
         long st = 0, e = start[n-1]+d, res = 0;
 
         while(st <= e){
