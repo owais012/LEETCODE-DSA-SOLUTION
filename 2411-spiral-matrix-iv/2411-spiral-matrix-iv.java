fa2aa8d42852a53ca2d83478408f7de8f94051ef
owais012 <146368386+owais012@users.noreply.github.com>
2024-09-09 23:06:29 +0530

    Time: 1110 ms (5.16%), Space: 63.6 MB (11.91%) - LeetHub

---
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.java
@@ -0,0 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public class Pair {
+        int row, col;
+
+        Pair(int r, int c) {
+            row = r;
+            col = c;
+        }
+    }
+
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int[][]res = new int[m][n];
+
+        Pair right = new Pair(0, n-1);
+        Pair down = new Pair(m-1, n-1);
+        Pair left = new Pair(m-1, 0);
+        Pair up = new Pair(0, 0);
+
+        var temp = head;
+
+        for(var row: res)Arrays.fill(row, -1);
+        outerLoop:
+        while(temp != null){
+            
+            for(int i = up.col; i <= right.col; i++){
+                res[up.row][i] = temp.val;
+                System.out.println(res[up.row][i] + " row -> " + up.row + " col-> "+i );
+                temp = temp.next;
+                if(temp == null) break outerLoop;
+            }
+
+            up.row +=1;
+            right.row +=1;
+            for(int i = right.row; i < down.row; i++){
+                res[i][right.col] = temp.val;
+                temp = temp.next;
+                System.out.println(res[i][right.col] + " row -> " + i + " col-> "+ right.col );
+
+                if(temp == null) break outerLoop;
+            }
+
+            // right.row += 1;
+            right.col -= 1;
+
+            for(int i = down.col; i> left.col; i--){
+                res[down.row][i] = temp.val;
+                temp = temp.next;
+                System.out.println(res[down.row][i] + " row -> " + down.row + " col-> "+i );
+
+                if(temp == null) break outerLoop;
+
+            }
+
+            down.row -= 1;
+            down.col -= 1;
+
+            for(int i = left.row; i>= up.row; i--){
+                res[i][left.col] = temp.val;
+                temp = temp.next;
+                System.out.println(res[i][left.col] + " row -> " + i + " col-> "+left.col );
+
+                if(temp == null) break outerLoop;
+
+            }
+
+            left.col += 1;
+            left.row -= 1;
+            up.col+=1;
+        }
+
+        return res;
+    }
+}
