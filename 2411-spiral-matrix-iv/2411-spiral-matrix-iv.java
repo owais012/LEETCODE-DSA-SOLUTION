e096337649f15a88419e73c15bea097701a82aa7
owais012 <146368386+owais012@users.noreply.github.com>
2024-09-10 00:02:32 +0530

    Time: 8 ms (37.87%), Space: 56.8 MB (35.73%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.java
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public class Pair {
         int row, col;
@@ -30,7 +20,8 @@ class Solution {
 
         for(var row: res)Arrays.fill(row, -1);
         outerLoop:
-        while(temp != null){
+        while(temp != null){   
+            // moving from the top left corner to the top right corner
             
             for(int i = up.col; i <= right.col; i++){
                 res[up.row][i] = temp.val;
@@ -38,25 +29,38 @@ class Solution {
                 if(temp == null) break outerLoop;
             }
 
+
+            //After traversing incrmenting up, and right 
             up.row +=1;
             right.row +=1;
+
+            //once top left -> right is complete then we go from the top right corner to the bottom right corner 
+
             for(int i = right.row; i < down.row; i++){
                 res[i][right.col] = temp.val;
                 temp = temp.next;
                 if(temp == null) break outerLoop;
             }
 
+            //once right side traversal is done we update the  Pair  right 
             right.col -= 1;
 
-            for(int i = down.col; i> left.col; i--){
+            // now once we are done teraversing from top right to bottom right 
+            // we mocve from the bottom right towards the bottom left
+
+             for(int i = down.col; i> left.col; i--){
                 res[down.row][i] = temp.val;
                 temp = temp.next;
                 if(temp == null) break outerLoop;
 
             }
-
+            
+            //now once , down row traversal is done we can update the Pair down
             down.row -= 1;
             down.col -= 1;
+
+
+            //now finall we move from the bottom left to up left
 
             for(int i = left.row; i>= up.row; i--){
                 res[i][left.col] = temp.val;
@@ -64,6 +68,7 @@ class Solution {
                 if(temp == null) break outerLoop;
             }
 
+            // once left bottom to up left is done we update the Pair left and up
             left.col += 1;
             left.row -= 1;
             up.col+=1;
