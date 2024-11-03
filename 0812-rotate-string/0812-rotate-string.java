ae220c3770147bc9d630336adfcbc45885457815
owais012 <146368386+owais012@users.noreply.github.com>
2024-11-03 20:13:28 +0530

    Time: 1 ms (15.9%), Space: 41.7 MB (20.3%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -8,12 +8,24 @@ class Solution {
 
         int n = s.length();
 
+        //Time complexity for this solution is -> (O(n^2))
+        //Brute force
+
         for(int i = 0; i < n-1; i += 1){
             var cur = s.substring(i+1, n) +  s.substring(0, i+1);
             if(cur.equals(goal))
                 return true;
         }
 
-        return false;
+        // return false;
+        //O(n) concatenate s with as s + s
+        //when a string is rotated then it always stay in that format 
+        // like for abcde -> (abcde)abcde a(bcdea)bcde ab(cdeab)cde abc(deabc)de
+        // abcd(eabcd)e these are all possible ways of rotation 
+
+        s = s+s;
+
+        return s.contains(goal);
+
     }
 }
