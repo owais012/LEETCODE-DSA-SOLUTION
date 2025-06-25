23205994ab1565fc345399337a4cd89d4faaa94a
Owais <146368386+owais012@users.noreply.github.com>
2025-06-25 17:59:33 +0530

    Time: 31 ms (5.44%), Space: 40.7 MB (95.92%) - LeetHub

---
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        long res = 0;
+
+        for(int i = 0; i <= Math.min(limit, n); i+= 1){
+            int b_start = Math.max(0, Math.min(limit, n-i-limit));
+
+            int b_end = Math.min(n, Math.min(limit, n-i));
+
+            // System.out.println(i +" "+ b_start + " " + b_end);
+            if(n-i-b_end <= limit)
+                res += (b_end-b_start+1);
+        }   
+
+        return res;
+    }
+}
