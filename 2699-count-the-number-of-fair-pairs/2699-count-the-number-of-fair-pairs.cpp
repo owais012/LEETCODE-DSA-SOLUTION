812c3e0e0d2ad2e6d88482d9e2e5f02cbf7cf752
Owais <146368386+owais012@users.noreply.github.com>
2025-08-04 23:39:20 +0530

    Time: 2956 ms (5.7%), Space: 71.6 MB (95.78%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,19 +1,39 @@
 #define ll long long
 class Solution {
 public:
-    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+    ll findPairs(ll target, vector<int>& nums){
+        int left = 0, right = nums.size()-1;
+        ll cnt = 0;
+        while(left < right){
+            while(left<right && (nums[left]+nums[right]) <= target)
+                left++;
+
+            cout<<right << " " << left <<" " << endl;
+            cnt += (right-left);
+            right--;
+        }
+
+        return cnt;
+    }
+
+    long long countFairPairs(vector<int>& nums, ll lower, ll upper) {
         ll cnt = 0;
         int n = nums.size();
+
+        //APPROACH 1. BINARY SEARCH
+
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n; i++){
             auto left = lower_bound(nums.begin()+i+1, nums.end(), lower-nums[i])-nums.begin();
             auto right = upper_bound(nums.begin()+i+1, nums.end(), upper-nums[i])-nums.begin();
             
             if(left==nums.size()) continue;
-            cout << right << " " << left << endl;
             cnt += (right- left); 
         }
 
-        return cnt;
+        // return cnt;
+
+        //APPROACH 2. TWO POINTER
+        return findPairs(lower-1, nums) - findPairs(upper, nums);
     }
 };
